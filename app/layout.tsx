e09fb768b5ba123d4aae5f7be888acacf85b3936
Phantozweb <iamsirenjeev@gmail.com>
2025-05-03 14:37:09 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Focus Axis',
+  description: 'Focus Axis is a web-based JCC simulator by Focus-in, built to help optometry students practice and understand cylinder axis and power refinement through interactive learning.',
   generator: 'v0.dev',
 }
 
